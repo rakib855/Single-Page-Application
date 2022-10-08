428eff7f313edcdb3ed84377690719ffff7720e7
Rakib <islamrakibul855@gmail.com>
2022-10-08 19:47:01 +0600

    Project

---
+++ Controllers/TransactionController.cs
@@ -0,0 +1,120 @@
+﻿using coreCrud.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static coreCrud.Helper;
+
+namespace coreCrud.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private readonly TransactionContext _context;
+
+        public TransactionController(TransactionContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Transaction
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.TransactionModels.ToListAsync());
+        }
+        public async Task<IActionResult> Create()
+        {
+            return View(await _context.TransactionModels.ToListAsync());
+        }
+
+        // GET: Transaction/AddOrEdit(Insert)
+        // GET: Transaction/AddOrEdit/5(Update)
+        [NoDirectAccess]
+        public async Task<IActionResult> AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+                return View(new TransactionModel());
+            else
+            {
+                var transactionModel = await _context.TransactionModels.FindAsync(id);
+                if (transactionModel == null)
+                {
+                    return NotFound();
+                }
+                return View(transactionModel);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit(int id, [Bind("TransactionId,AccountNumber,BeneficiaryName,BankName,SWIFTCode,Amount,Date")] TransactionModel transactionModel)
+        {
+            if (ModelState.IsValid)
+            {
+                //Insert
+                if (id == 0)
+                {
+                    transactionModel.Date = DateTime.Now;
+                    _context.Add(transactionModel);
+                    await _context.SaveChangesAsync();
+
+                }
+                //Update
+                else
+                {
+                    try
+                    {
+                        _context.Update(transactionModel);
+                        await _context.SaveChangesAsync();
+                    }
+                    catch (DbUpdateConcurrencyException)
+                    {
+                        if (!TransactionModelExists(transactionModel.TransactionId))
+                        { return NotFound(); }
+                        else
+                        { throw; }
+                    }
+                }
+                return Json(new { isValid = true, html = Helper.RenderRazorViewToString(this, "_ViewAll", _context.TransactionModels.ToList()) });
+            }
+            return Json(new { isValid = false, html = Helper.RenderRazorViewToString(this, "AddOrEdit", transactionModel) });
+        }
+
+        // GET: Transaction/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var transactionModel = await _context.TransactionModels
+                .FirstOrDefaultAsync(m => m.TransactionId == id);
+            if (transactionModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(transactionModel);
+        }
+
+        // POST: Transaction/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var transactionModel = await _context.TransactionModels.FindAsync(id);
+            _context.TransactionModels.Remove(transactionModel);
+            await _context.SaveChangesAsync();
+            return Json(new { html = Helper.RenderRazorViewToString(this, "_ViewAll", _context.TransactionModels.ToList()) });
+        }
+
+        private bool TransactionModelExists(int id)
+        {
+            return _context.TransactionModels.Any(e => e.TransactionId == id);
+        }
+    }
+
+}
+
---
+++ Models/TransactionContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace coreCrud.Models
+{
+    public class TransactionContext: DbContext
+    {
+        public TransactionContext(DbContextOptions<TransactionContext> options) : base(options)
+        { }
+        public DbSet<TransactionModel> TransactionModels { get; set; }
+
+    }
+}
